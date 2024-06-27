590402117dcc4b2411bd911ae6c6c6b1ba182e9c
Reshie <reo040125@gmail.com>
2024-06-27 14:24:10 +0900

    change structure of directory

--- app/ui/crew/post-button.tsx
+++ app/ui/crew/post-button.tsx
@@ -8,7 +8,7 @@ export default function PostButton() {
     <div className="fixed bottom-8 right-8 w-12 h-12 rounded-full bg-sky-500 hover:bg-sky-400 flex justify-center items-center">
       <Link
         key="Post"
-        href="/home/post"
+        href="/post"
       >
         <PlusIcon className="size-8 stroke-2 text-white"/>
       </Link>
